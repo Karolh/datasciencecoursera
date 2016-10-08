72d24ac4aec44961fd27f9eadf54c8f06b08b99d
karol <karol.murray@sonalake.com>
2016-10-08 11:47:36 +0100

    complete.R is outputting the id and the number of good cases per csv file

---
+++ complete.R
@@ -0,0 +1,29 @@
+complete <- function(directory, id = 1:332) {
+    data <- matrix(nrow = length(id), ncol = 2 )
+    rowCount <- 1
+    for (index in id){
+        allCases <- readDataFile(directory, index)
+        number <- numberOfGoodCases(allCases)
+        
+        data[rowCount, 1] <- index
+        data[rowCount, 2] <- number
+        rowCount <- rowCount + 1
+    }
+    data
+}
+
+numberOfGoodCases <- function(allCases){
+    completeCases <- complete.cases(allCases)
+    goodCases <- allCases[completeCases, ]
+    nrow(goodCases)
+}
+
+readDataFile <- function(directory, index) {
+    if (index < 10){
+        index <- paste("00", index, sep = "")
+    } else if (index >= 10 & index < 100){
+        index <- paste("0", index, sep = "")
+    }
+    file <- paste(directory, "/", index, ".csv", sep = "")
+    read.csv(file)
+}
